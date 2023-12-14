bfa6f69bb336890155c2e815d4f968843da5384d
hadilouakk <hadil2001ama@gmail.com>
2023-12-14 14:04:25 +0100

    inite

--- src/pages/CreerUnComptePage/CreerUnComptePage.jsx
+++ src/pages/CreerUnComptePage/CreerUnComptePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CreerUnComptePage() {
   return (
     <div>
-      <h1>compte</h1>
+      <h1>Compte</h1>
     </div>
   )
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import styles from './HomePage.module.css'
 
 export default function HomePage() {
   return (
-    <div>
+  
+    <div  className={styles.HomePage}>
       <h1>HomePage</h1>
     </div>
+    
   )
 }
