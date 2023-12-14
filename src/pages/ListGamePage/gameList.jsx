f909af8237ec93c8330b1a6309994bebad542ac9
Alyssa <alyssapaygambar@gmail.com>
2023-12-14 12:52:04 +0100

    Integration_img

---
+++ src/pages/ListGamePage/gameList.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styles from './GameList.module.css'
+
+export default function GameList() {
+
+    return <div class={ styles.grid }>
+        <div class={ styles.item }><a href="#"><img src="/img/cod.jpg" width="150px"/></a></div>
+        <div class={ styles.item }><a href="#"><img src="/img/valo.png" width="150px" /></a></div>
+        <div class={ styles.item }><a href="#"><img src="img/fortnite.webp" width="150px"/></a></div>
+        <div class={ styles.item }><a href="#"><img src="/img/doom.jpg" width="150px"/></a></div>
+        <div class={ styles.item }><a href="#"><img src="img/lol.jpg" width="150px"/></a></div>
+        <div class={ styles.item }><a href="#"><img src="img/st.jpg" width="150px"/></a></div>
+    </div>
+
+//   return <center> <table>
+//         <tr><h1>Tournament Available </h1></tr>
+//         <tr>
+//             <td><a href="#"><img src="img/cod.jpg" width="150px"/></a></td>
+//             <td><a href="#"><img src="img/valo.png" width="150px" /></a></td>
+//             <td><a href="#"><img src="img/fortnite.webp" width="150px"/></a></td>
+            
+//         </tr>
+
+//         <tr>
+//             <td><a href="#">Call of Duty</a></td>
+//             <td><a href="#">Valorant</a></td>
+//             <td><a href="#">Fortnite</a></td>
+            
+//             <td></td>
+//         </tr>
+
+//         <tr>
+//             <td><a href="#"><img src="/img/doom.jpg" width="150px"/></a></td>
+//             <td><a href="#"><img src="img/lol.jpg" width="150px"/></a></td>
+//             <td><a href="#"><img src="img/st.jpg" width="150px"/></a></td>
+//         </tr>
+
+//         <tr>
+//             <td><a href="#">Doom</a></td>
+//             <td><a href="#">League of legends</a></td>
+//             <td><a href="#">Street Fighters</a></td>
+//         </tr>
+//     </table>
+//     </center>
+
+}
--- src/pages/ListesJeuxPage/ListesJeuxPage.jsx
+++ src/pages/ListesJeuxPage/ListesJeuxPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import GameList from '../ListGamePage/gameList'
 
 export default function ListesJeuxPage() {
   return (
     <div>
       <h1>Listes des jeux </h1>
+      <GameList />
     </div>
   )
 }
